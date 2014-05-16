0b4f6b77dadab358cb68a127be5a5b3c34a35015
Yury Tarabanko <tarabanko@mirapolis.ru>
2014-05-16 09:51:46 +0400

    testing travis ci

--- console.face.js
+++ console.face.js
@@ -2,6 +2,19 @@
 require('./src/face');
 
 },{"./src/face":2}],2:[function(require,module,exports){
-console.face = function(){}; 
+console.face = function(name){
+    var face = getFace(name);
+    face && face();
+};
 
+var facesCache = {};
+
+function getFace(name) {
+    var face = facesCache[name];
+    if(!face){
+        console.log('Need to load');    
+    }
+    
+    return face;
+}
 },{}]},{},[1])
--- src/face.js
+++ src/face.js
@@ -1,1 +1,15 @@
-console.face = function(){}; 
+console.face = function(name){
+    var face = getFace(name);
+    face && face();
+};
+
+var facesCache = {};
+
+function getFace(name) {
+    var face = facesCache[name];
+    if(!face){
+        console.log('Need to load');    
+    }
+    
+    return face;
+}
